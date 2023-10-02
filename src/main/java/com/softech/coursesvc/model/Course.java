cb4746822aa1d281ead6beb29ab76ec13f67baa0
heenakh30 <116773819+heenakh30@users.noreply.github.com>
2023-10-02 19:58:41 -0400

    Added course class

---
+++ src/main/java/com/softech/coursesvc/model/Course.java
@@ -0,0 +1,12 @@
+package com.softech.coursesvc.model;
+
+public class Course {
+
+    long courseId;// primary key
+    String courseName;
+    boolean isActive;
+    double fees;
+    String currency;
+    int days;
+    String type;
+}
