31e8ba16ca8c3444a0b67e051d2de18ec724d5c1
Richa Luthra <richaluthra.ca@gmail.com>
2023-10-02 17:56:47 -0400

    initial commit

---
+++ src/main/java/com/softech/coursesvc/CoursesvcApplication.java
@@ -0,0 +1,13 @@
+package com.softech.coursesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softech/coursesvc/CoursesvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softech.coursesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
