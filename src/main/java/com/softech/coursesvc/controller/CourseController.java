ac7b385286299b831ce27811d7a824ba596fd469
Richa Luthra <richaluthra.ca@gmail.com>
2023-10-02 19:35:00 -0400

    second  commit

--- src/main/java/com/softech/coursesvc/controller/CourseController.java
+++ src/main/java/com/softech/coursesvc/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseController {
 
 
+    //dummy endpoint
     @GetMapping("/")
     public String hello(){
         return "Hello!!!";
