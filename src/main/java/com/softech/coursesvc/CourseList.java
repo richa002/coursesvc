6e4b206b3bdcee57b68b1e3b42a51f4aa06b499a
heenakh30 <116773819+heenakh30@users.noreply.github.com>
2023-10-11 12:17:49 -0400

    Addressed PR comments

---
+++ src/main/java/com/softech/coursesvc/CourseList.java
@@ -0,0 +1,10 @@
+package com.softech.coursesvc;
+
+import com.softech.coursesvc.model.Course;
+
+import java.util.List;
+
+public class CourseList {
+    List<Course> courses;
+    int noOfRecords;
+}
--- src/main/java/com/softech/coursesvc/controller/CourseController.java
+++ src/main/java/com/softech/coursesvc/controller/CourseController.java
@@ -1,9 +1,11 @@
 package com.softech.coursesvc.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class CourseController {
 
 
