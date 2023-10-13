8553b469f138d6005365faeb8bab95c97fb54226
Tavneet1430 <120430519+Tavneet1430@users.noreply.github.com>
2023-10-13 14:38:21 -0400

    Your commit message here

--- src/main/java/com/softech/coursesvc/model/Course.java
+++ src/main/java/com/softech/coursesvc/model/Course.java
@@ -1,6 +1,21 @@
 package com.softech.coursesvc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Course {
+    @Id
 
     private long courseId;// primary key
     private String courseName;
