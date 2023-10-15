3a9457f41ebf458d1dc561a534e2eb85b2876a07
heenakh30 <116773819+heenakh30@users.noreply.github.com>
2023-10-15 16:19:29 -0400

    Add pending files

--- src/main/java/com/softech/coursesvc/CourseList.java
+++ src/main/java/com/softech/coursesvc/CourseList.java
@@ -4,6 +4,7 @@ import com.softech.coursesvc.entity.CourseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
@@ -11,6 +12,6 @@ import java.util.List;
 @NoArgsConstructor
 
 public class CourseList {
-    List<CourseEntity> courses;
-    int noOfRecords;
+    private List<CourseEntity> courses;
+    private int noOfRecords;
 }
---
+++ src/main/java/com/softech/coursesvc/service/CourseSvc.java
@@ -0,0 +1,18 @@
+package com.softech.coursesvc.service;
+
+import com.softech.coursesvc.entity.CourseEntity;
+import com.softech.coursesvc.repository.CourseRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseSvc {
+    @Autowired
+    private CourseRepos courseRepos;
+
+    public List<CourseEntity> getAllCourses() {
+        return courseRepos.findAll();
+    }
+}
