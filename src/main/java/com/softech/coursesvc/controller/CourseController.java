c5d8a2f004fb48c72d0aef30a203bfb67e8ddedd
Richa Luthra <richaluthra.ca@gmail.com>
2023-10-15 16:36:47 -0400

    cleaning the mess

--- src/main/java/com/softech/coursesvc/controller/CourseController.java
+++ src/main/java/com/softech/coursesvc/controller/CourseController.java
@@ -1,11 +1,14 @@
 package com.softech.coursesvc.controller;
 
-import com.softech.coursesvc.courseService.CourseSvc;
-
+import com.softech.coursesvc.CourseList;
+import com.softech.coursesvc.entity.CourseEntity;
+import com.softech.coursesvc.service.CourseSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/coursesvc")
@@ -18,6 +21,16 @@ public class CourseController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello!!!";
+    }
+
+    @GetMapping("/v1/course")
+    public CourseList getCourses() {
+        CourseList courseList = new CourseList();
+        List<CourseEntity> courses = courseSvc.getAllCourses();
+        courseList.setCourses(courses);
+        courseList.setNoOfRecords(courses.size());
+        return courseList;
+
     }
 
 
