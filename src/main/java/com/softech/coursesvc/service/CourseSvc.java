87ab90abcd6c5b9f46f2f92aa830f85b2cc39d21
Neetu <makhijaneetu@ymail.com>
2023-10-15 21:49:00 -0400

    GETBYId done

--- src/main/java/com/softech/coursesvc/service/CourseSvc.java
+++ src/main/java/com/softech/coursesvc/service/CourseSvc.java
@@ -1,6 +1,7 @@
 package com.softech.coursesvc.service;
 
 import com.softech.coursesvc.entity.CourseEntity;
+import com.softech.coursesvc.model.Course;
 import com.softech.coursesvc.repository.CourseRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,10 @@ public class CourseSvc {
 
     public List<CourseEntity> getAllCourses() {
         return courseRepos.findAll();
+    }
+
+
+    public CourseEntity getCourseById(Long courseId) {
+        return courseRepos.findById(courseId).orElse(null);
     }
 }
