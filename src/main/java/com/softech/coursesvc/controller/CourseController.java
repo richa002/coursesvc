f41bc05d1c0bcfe53c1580e2bb5dd4dd67bf4608
Tavneet1430 <120430519+Tavneet1430@users.noreply.github.com>
2023-10-17 22:26:54 -0400

    create, update and delete function created

--- src/main/java/com/softech/coursesvc/controller/CourseController.java
+++ src/main/java/com/softech/coursesvc/controller/CourseController.java
@@ -7,10 +7,7 @@ import com.softech.coursesvc.service.CourseSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,16 +35,29 @@ public class CourseController {
     }
 
 @GetMapping("/v1/course/{courseId}")
-public ResponseEntity<CourseEntity> getCourseById(@PathVariable Long courseId)
-{
-    CourseEntity course= courseSvc.getCourseById(courseId);
-    if(course != null)
-    {
+public ResponseEntity<CourseEntity> getCourseById(@PathVariable Long courseId) {
+    CourseEntity course = courseSvc.getCourseById(courseId);
+    if (course != null) {
         return new ResponseEntity<CourseEntity>(course, HttpStatus.OK);
-    }else {
+    } else {
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 
 
+    }}
+
+    @PostMapping("/v1/course")
+    public CourseEntity createCourse(@RequestBody CourseEntity courseEntity){
+        return courseSvc.createCourse(courseEntity);
+
     }
-}
-}
+    @PutMapping("/v1/course/{courseId}")
+    public void updateCourse(@PathVariable Long courseId, @RequestBody CourseEntity courseEntity){
+        courseSvc.updateCourse(courseId, courseEntity);
+    }
+
+    @DeleteMapping("/v1/course/{courseId}")
+    public void deleteCourse(@PathVariable Long courseId){
+        courseSvc.deleteCourse(courseId);
+
+    }}
+
