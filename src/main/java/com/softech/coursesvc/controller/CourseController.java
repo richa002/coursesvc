c322b241f94154b4274d508c088f5fa3131520e6
heenakh30 <116773819+heenakh30@users.noreply.github.com>
2023-10-24 21:58:55 -0400

    Sep22 13 (#10)
    * Added course class
    
    * Added course class
    
    * Addressed PR comments
    
    * Addressed PR comments
    
    * Resolved PR comments
    
    * Resolved PR comments
    
    * Add pending files
    
    * Created Exception Handler

--- src/main/java/com/softech/coursesvc/controller/CourseController.java
+++ src/main/java/com/softech/coursesvc/controller/CourseController.java
@@ -2,7 +2,6 @@ package com.softech.coursesvc.controller;
 
 import com.softech.coursesvc.CourseList;
 import com.softech.coursesvc.entity.CourseEntity;
-import com.softech.coursesvc.model.Course;
 import com.softech.coursesvc.service.CourseSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,14 +24,15 @@ public class CourseController {
     }
 
     @GetMapping("/v1/course")
-    public CourseList getCourses() {
+    public ResponseEntity<CourseList> getCourses() {
         CourseList courseList = new CourseList();
         List<CourseEntity> courses = courseSvc.getAllCourses();
         courseList.setCourses(courses);
         courseList.setNoOfRecords(courses.size());
-        return courseList;
+        return ResponseEntity.ok(courseList);
 
     }
+
 
 @GetMapping("/v1/course/{courseId}")
 public ResponseEntity<CourseEntity> getCourseById(@PathVariable Long courseId) {
